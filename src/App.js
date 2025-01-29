4dcd850a0e01f33da1ae287c8316a317875d48f9
OmarSalem123 <omar123789ali@gmail.com>
2025-01-29 12:37:00 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {
-	closestCorners,
-	DndContext,
-	MouseSensor,
-	useSensor,
-	useSensors,
+  closestCorners,
+  DndContext,
+  MouseSensor,
+  useSensor,
+  useSensors,
 } from "@dnd-kit/core";
 import "./App.css";
 import { useRef, useState } from "react";
@@ -14,273 +14,308 @@ import Header from "./Components/Header/Header";
 import DashBoardMenu from "./Components/DashBoardMenu/DashBoardMenu";
 
 function App() {
-	let [data, setData] = useState([
-		{
-			id: 1,
-			chartType: "LabelChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 2,
-			chartType: "LabelChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 3,
-			chartType: "LabelChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 4,
-			chartType: "LabelChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 5,
-			chartType: "LineChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 6,
-			chartType: "LineChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 7,
-			chartType: "LineChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-		{
-			id: 8,
-			chartType: "LineChart",
-			style: "col-lg-3",
-			ChartData: {
-				labels: [
-					"January",
-					"February",
-					"March",
-					"April",
-					"May",
-					"June",
-					"July",
-				],
-				data: [65, 59, 80, 81, 56, 55, 40],
-			},
-		},
-	]);
-	const [mainData, setMainData] = useState([
-		{
-			id: 1,
-			chartType: "LabelChart",
-			style: "col-lg-3",
-			img: "labelchart.png",
-		},
-		{ id: 2, chartType: "LineChart", style: "col-lg-3", img: "labelchart.png" },
-		{ id: 3, chartType: "BarChart", style: "col-lg-3", img: "labelchart.png" },
-		{ id: 4, chartType: "PieChart", style: "col-lg-3", img: "labelchart.png" },
-	]); //Side Menu Chart types
+  let [data, setData] = useState([
+    {
+      id: 1,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 2,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 3,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 4,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 5,
+      chartType: "LineChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 6,
+      chartType: "LineChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 7,
+      chartType: "LineChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+    {
+      id: 8,
+      chartType: "LineChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    },
+  ]);
+  const [mainData, setMainData] = useState([
+    {
+      id: 1,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      img: "labelchart.png",
+      type: "templete",
+    },
+    {
+      id: 2,
+      chartType: "LineChart",
+      style: "col-lg-3",
+      img: "labelchart.png",
+      type: "templete",
+    },
+    {
+      id: 3,
+      chartType: "BarChart",
+      style: "col-lg-3",
+      img: "labelchart.png",
+      type: "templete",
+    },
+    {
+      id: 4,
+      chartType: "PieChart",
+      style: "col-lg-3",
+      img: "labelchart.png",
+      type: "templete",
+    },
+    {
+      id: 5,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      img: "chart.svg",
+      type: "custom",
+    },
+    {
+      id: 6,
+      chartType: "LineChart",
+      style: "col-lg-3",
+      img: "chart.svg",
+      type: "custom",
+    },
+    {
+      id: 7,
+      chartType: "BarChart",
+      style: "col-lg-3",
+      img: "chart.svg",
+      type: "custom",
+    },
+    {
+      id: 8,
+      chartType: "PieChart",
+      style: "col-lg-3",
+      img: "chart.svg",
+      type: "custom",
+    },
+  ]); //Side Menu Chart types
 
-	const [customData, setCustomData] = useState([
-		{
-			id: 5,
-			chartType: "LabelChart",
-			style: "col-lg-3",
-			img: "chart.svg",
-		},
-		{ id: 6, chartType: "LineChart", style: "col-lg-3", img: "chart.svg" },
-		{ id: 7, chartType: "BarChart", style: "col-lg-3", img: "chart.svg" },
-		{ id: 8, chartType: "PieChart", style: "col-lg-3", img: "chart.svg" },
-	]);
-	const getTaskPos = (id) => data.findIndex((task) => task.id === id);
+  const getTaskPos = (id) => data.findIndex((task) => task.id === id);
 
-	const handleDragEnd = (event) => {
-		const { active, over } = event;
-		if (!over || active.id === over.id) return;
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+    if (!over || active.id === over.id) return;
 
-		const activeIndex = getTaskPos(active.id);
-		const overIndex = getTaskPos(over.id);
+    const activeIndex = getTaskPos(active.id);
+    const overIndex = getTaskPos(over.id);
 
-		setData((tasks) => {
-			const updatedTasks = [...tasks];
-			[updatedTasks[activeIndex], updatedTasks[overIndex]] = [
-				updatedTasks[overIndex],
-				updatedTasks[activeIndex],
-			];
-			return updatedTasks;
-		});
-	};
+    setData((tasks) => {
+      const updatedTasks = [...tasks];
+      [updatedTasks[activeIndex], updatedTasks[overIndex]] = [
+        updatedTasks[overIndex],
+        updatedTasks[activeIndex],
+      ];
+      return updatedTasks;
+    });
+  };
 
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const itemData = e.dataTransfer.getData("text/plain");
+    const chartType = JSON.parse(itemData);
 
-	const handleDrop = (e) => {
-		e.preventDefault();
-		const itemData = e.dataTransfer.getData("text/plain");
-		const chartType = JSON.parse(itemData);
+    if (!chartType) return;
+    const newId = Date.now();
+    const randomData = Array.from({ length: 7 }, () =>
+      Math.floor(Math.random() * 100)
+    );
+    const labels =
+      data.length > 0
+        ? data[0].ChartData.labels
+        : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
 
-		if (!chartType) return;
-		const newId = Date.now();
-		const randomData = Array.from({ length: 7 }, () =>
-			Math.floor(Math.random() * 100)
-		);
-		const labels =
-			data.length > 0
-				? data[0].ChartData.labels
-				: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+    const chartStyle =
+      mainData.find((item) => item.chartType === chartType)?.style ||
+      "col-lg-3";
 
-		const chartStyle =
-			mainData.find((item) => item.chartType === chartType)?.style ||
-			"col-lg-3";
+    const newChart = {
+      id: newId,
+      chartType,
+      style: chartStyle,
+      ChartData: {
+        labels,
+        data: randomData,
+      },
+    };
+    setData((prev) => [...prev, newChart]);
+  };
 
-		const newChart = {
-			id: newId,
-			chartType,
-			style: chartStyle,
-			ChartData: {
-				labels,
-				data: randomData,
-			},
-		};
-		setData((prev) => [...prev, newChart]);
-	};
-
-	const handleDragOver = (e) => {
-		e.preventDefault();
-		e.dataTransfer.dropEffect = "copy";
-	};
-	const containerRef = useRef(null);
-	const sensors = useSensors(
-		useSensor(MouseSensor, {
-			activationConstraint: { delay: 100 },
-		})
-	);
-	return (
-		<div className="App container-fluid">
-			<Header
-				title={"Dynamic Dashboard"}
-				subTitle={"Create Your customied dashboard now"}
-			>
-				<DashBoardMenu mainData={mainData} customData={customData} />
-			</Header>
-			<div
-				ref={containerRef}
-				onDrop={handleDrop}
-				onDragOver={handleDragOver}
-				style={{
-					border: "2px dashed #007bff",
-					padding: "20px",
-					marginTop: "20px",
-					maxHeight: "fit-content",
-					backgroundColor: "#f8f9fa",
-					overflow: "hidden",
-				}}
-			>
-				<div className="row">
-					<DndContext
-						collisionDetection={closestCorners}
-						modifiers={[restrictToWindowEdges]}
-						onDragEnd={handleDragEnd}
-						sensors={sensors}
-					>
-						<SortableContext
-							items={data.map((i) => i.id)}
-							strategy={rectSortingStrategy}
-						>
-							{data.map((i) => {
-								return (
-									<div key={i.id} className={i.style}>
-										<Cards id={i.id} item={i} setData={setData} data={data} />
-									</div>
-								);
-							})}
-						</SortableContext>
-					</DndContext>
-				</div>
-			</div>
-		</div>
-	);
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "copy";
+  };
+  const containerRef = useRef(null);
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      activationConstraint: { delay: 100 },
+    })
+  );
+  return (
+    <div className="App container-fluid">
+      <Header
+        title={"Dynamic Dashboard"}
+        subTitle={"Create Your customied dashboard now"}
+      >
+        <DashBoardMenu mainData={mainData} />
+      </Header>
+      <div
+        ref={containerRef}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        style={{
+          border: "2px dashed #007bff",
+          padding: "20px",
+          marginTop: "20px",
+          maxHeight: "fit-content",
+          backgroundColor: "#f8f9fa",
+          overflow: "hidden",
+        }}
+      >
+        <div className="row">
+          <DndContext
+            collisionDetection={closestCorners}
+            modifiers={[restrictToWindowEdges]}
+            onDragEnd={handleDragEnd}
+            sensors={sensors}
+          >
+            <SortableContext
+              items={data.map((i) => i.id)}
+              strategy={rectSortingStrategy}
+            >
+              {data.map((i) => {
+                return (
+                  <div key={i.id} className={i.style}>
+                    <Cards id={i.id} item={i} setData={setData} data={data} />
+                  </div>
+                );
+              })}
+            </SortableContext>
+          </DndContext>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/DashBoardMenu/DashBoardMenu.jsx
+++ src/Components/DashBoardMenu/DashBoardMenu.jsx
@@ -2,49 +2,52 @@ import React, { useState } from "react";
 import OffCanvasTemplate from "../OffCanvas";
 import DashboardSelection from "./DashboardSelection/DashboardSelection";
 
-export default function DashBoardMenu({ mainData, customData }) {
-	const handleDragStart = (e, item) => {
-		e.dataTransfer.setData("text/plain", JSON.stringify(item));
-	};
-	const [choice, setChoice] = useState("ready");
-	let data = choice === "ready" ? mainData : customData;
-	return (
-		<div className="d-flex justify-content-center align-items-center">
-			<OffCanvasTemplate
-				ButtonText={"Generate New Dashboard"}
-				title={"Select widgets"}
-				backdrop={false}
-			>
-				<DashboardSelection choice={choice} setChoice={setChoice} />
+export default function DashBoardMenu({ mainData }) {
+  const handleDragStart = (e, item) => {
+    e.dataTransfer.setData("text/plain", JSON.stringify(item));
+  };
+  const [choice, setChoice] = useState("ready");
 
-				<div className="container">
-					<div className="row ">
-						{data.map((item) => (
-							<div key={item.id} className="col-lg-4">
-								<div
-									className="item-card"
-									draggable
-									onDragStart={(e) => handleDragStart(e, item.chartType)}
-									style={{
-										padding: "10px",
-										margin: "5px 0",
-										backgroundColor: "#f8f9fa",
-										border: "1px solid #ddd",
-										borderRadius: "4px",
-										cursor: "grab",
-									}}
-								>
-									<img
-										src={item.img}
-										alt={item.chartType}
-										className="img-fluid"
-									/>
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-			</OffCanvasTemplate>
-		</div>
-	);
+  const data = mainData.filter((item) =>
+    choice === "ready" ? item.type === "templete" : item.type === "custom"
+  );
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <OffCanvasTemplate
+        ButtonText={"Generate New Dashboard"}
+        title={"Select widgets"}
+        backdrop={false}
+      >
+        <DashboardSelection choice={choice} setChoice={setChoice} />
+
+        <div className="container">
+          <div className="row ">
+            {data.map((item) => (
+              <div key={item.id} className="col-lg-4">
+                <div
+                  className="item-card"
+                  draggable
+                  onDragStart={(e) => handleDragStart(e, item.chartType)}
+                  style={{
+                    padding: "10px",
+                    margin: "5px 0",
+                    backgroundColor: "#f8f9fa",
+                    border: "1px solid #ddd",
+                    borderRadius: "4px",
+                    cursor: "grab",
+                  }}
+                >
+                  <img
+                    src={item.img}
+                    alt={item.chartType}
+                    className="img-fluid"
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </OffCanvasTemplate>
+    </div>
+  );
 }
