c2ee51d7896d7db782a25c7ff9ce00e00c641451
Joe-A11-RM <yousefsherif7500@gmail.com>
2025-01-30 15:43:32 +0200

    11th Commit

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,45 +1,17 @@
-import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
 import { BiSolidTrashAlt } from "react-icons/bi";
 import React from "react";
 import LabelChart from "../Charts/LabelChart";
 import PieChart from "../Charts/PieChart";
 
-export default function Cards({ item, setData, data }) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: item.id });
-  const customTransition = transition
-    ? transition.replace("250ms", "600ms")
-    : "transform 200ms ease";
-
-  const style = {
-    transition: customTransition,
-    transform: CSS.Transform.toString(transform),
-  };
-
-  const removeWidget = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-
-  const handleBinClick = (e) => {
+export default function Cards({ item, i, removeWidget }) {
+  const handleDelete = (e) => {
     e.stopPropagation();
-    removeWidget(item.id);
-  };
-  const handlePointerDown = (e) => {
-    if (e.target.closest(".bin")) {
-      e.preventDefault();
-    }
+    console.log("ID", i);
+    removeWidget(i);
   };
 
   return (
-    <div
-      key={item.id}
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-      style={style}
-      onPointerDown={handlePointerDown}
-    >
+    <div key={item.id}>
       {item.chartType === "LabelChart" ? (
         <LabelChart
           data={item.ChartData.data}
@@ -47,7 +19,7 @@ export default function Cards({ item, setData, data }) {
           title={item.ChartData.title}
           number={item.ChartData.number}
           color={item.ChartData.color}
-          onDelete={handleBinClick}
+          onDelete={handleDelete}
         />
       ) : item.chartType === "PieChart" ? (
         <PieChart
@@ -56,11 +28,11 @@ export default function Cards({ item, setData, data }) {
           title={item.ChartData.title}
           number={item.ChartData.number}
           color={item.ChartData.color}
-          onDelete={handleBinClick}
+          onDelete={handleDelete}
         />
       ) : (
         <div className="item-card">
-          <div className="bin" onClick={handleBinClick}>
+          <div className="bin" onClick={handleDelete}>
             <BiSolidTrashAlt />
           </div>
           <div className="item-card-chart">{item.chartType}</div>
--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,32 +1,236 @@
 import React, { useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
+import Cards from "../Cards/Cards";
 
 const ReactGridLayout = WidthProvider(Responsive);
 
-export default function NonResizableGrid() {
-  const [layout, setLayout] = useState([
-    { i: "1", x: 0, y: 0, w: 2, h: 2 },
-    { i: "2", x: 2, y: 0, w: 2, h: 2 },
+export default function Grid() {
+  let [data, setData] = useState([
+    {
+      id: 1,
+      chartType: "LabelChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: Array.from({ length: 7 }, () => Math.floor(Math.random() * 100)),
+        color: "#3B82F6",
+        number: Math.floor(Math.random() * 100),
+        title: "Revenue Growth",
+      },
+    },
+    {
+      id: 1,
+      chartType: "PieChart",
+      style: "col-lg-3",
+      ChartData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        data: Array.from({ length: 7 }, () => Math.floor(Math.random() * 100)),
+        color: "#3B82F6",
+        number: Math.floor(Math.random() * 100),
+        title: "Revenue Growth",
+      },
+    },
   ]);
+  const [layout, setLayout] = useState([
+    {
+      i: "1",
+      x: 0,
+      y: 0,
+      w: 3,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[0]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "2",
+      x: 3,
+      y: 0,
+      w: 4,
+      h: 2,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[1]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "3",
+      x: 0,
+      y: 1,
+      w: 3,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[0]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "4",
+      x: 7,
+      y: 0,
+      w: 3,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[0]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "5",
+      x: 7,
+      y: 1,
+      w: 3,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[0]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "6",
+      x: 12,
+      y: 0,
+      w: 2,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[0]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "7",
+      x: 12,
+      y: 1,
+      w: 2,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[0]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+    {
+      i: "8",
+      x: 0,
+      y: 2,
+      w: 6,
+      h: 1,
+      component: (
+        <div>
+          <Cards
+            i={`new_${Date.now()}`}
+            item={data[1]}
+            setData={setData}
+            data={data}
+          />
+        </div>
+      ),
+    },
+  ]);
+  const removeWidget = (id) => {
+    setLayout((prevLayout) => prevLayout.filter((item) => item.i !== id));
+  };
+  const handleDrop = (layout, layoutItem, event) => {
+    const chartType = event.dataTransfer.getData("chartType");
+    const chartData = data.find((item) => item.chartType === chartType);
+
+    if (chartData) {
+      const newItem = {
+        i: `new_${Date.now()}`,
+        x: layoutItem.x,
+        y: layoutItem.y,
+        w: chartData.chartType === "LabelChart" ? 3 : 4,
+        h: chartData.chartType === "LabelChart" ? 1 : 2,
+        component: (
+          <Cards
+            i={`new_${Date.now()}`}
+            item={chartData}
+            setData={setData}
+            data={data}
+            layout={layout}
+            setlayout={setLayout}
+            removeWidget={removeWidget}
+          />
+        ),
+      };
+      setLayout((prev) => [...prev, newItem]);
+    }
+  };
 
   return (
     <div>
       <ReactGridLayout
-        className="layout"
-        cols={{ lg: 12 }}
-        rowHeight={30}
+        className="dashboard-drop-area"
+        cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
         width={1200}
-        layout={layout}
-        isResizable={false} // Disable resizing
-        onLayoutChange={(newLayout) => setLayout(newLayout)}
+        isResizable={false}
+        isDroppable={true}
+        onDrop={handleDrop}
+        allowOverlap={false}
+        autoSize={true}
+        onLayoutChange={(newLayout) => console.log("New Layout: ", newLayout)}
       >
         {layout.map((item) => (
-          <div
-            key={item.i}
-            data-grid={item}
-            style={{ background: "lightgray" }}
-          >
-            Component {item.i}
+          <div key={item.i} data-grid={item}>
+            {item.component}
           </div>
         ))}
       </ReactGridLayout>
