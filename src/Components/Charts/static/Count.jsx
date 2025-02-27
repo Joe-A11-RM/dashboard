80162f3d1449398f6ffb6f6dee8dadc96eb524c5
Joe-A11-RM <yousefsherif7500@gmail.com>
2025-02-27 15:28:20 +0200

    26th Commit

--- src/Components/Charts/static/Count.jsx
+++ src/Components/Charts/static/Count.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { LuExternalLink } from "react-icons/lu";
 
-const Count = ({ title, number }) => {
-  return (
-    <div className="count">
-      <div className="count-header">
-        <p>{title}</p>
-        <LuExternalLink color="#CCCCCC" size={16} className="count-link" />
-      </div>
-      <p className="count-number">{number}</p>
-    </div>
-  );
+const Count = ({ title, number, key }) => {
+	return (
+		<div key={key} className="count">
+			<div className="count-header">
+				<p>{title}</p>
+				<LuExternalLink color="#CCCCCC" size={16} className="count-link" />
+			</div>
+			<p className="count-number">{number}</p>
+		</div>
+	);
 };
 
 export default Count;
